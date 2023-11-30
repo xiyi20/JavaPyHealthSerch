fa4b049a4f289885de346d0fb899b447c6f31e79
dxcxy <wanandsc@gmail.com>
2023-11-30 11:33:34 +0800

    完善基本功能

--- getdata.py
+++ getdata.py
@@ -8,10 +8,14 @@ option=webdriver.ChromeOptions()
 option.add_argument('--log-level=3')
 
 borwers=webdriver.Chrome(options=option)
+
 borwers.get('https://jibing.bmcx.com/')
 
 buttom=input()
-# print(buttom)
-borwers.find_element(By.XPATH,'//*[@id="x_z_k"]/ul[1]/li['+buttom+']/label/input').click()
+values=buttom.split(',')
+for i in values:
+    if i=='':
+        continue
+    borwers.find_element(By.XPATH,'//*[@id="x_z_k"]/ul[1]/li['+i+']/label/input').click()
 
 time.sleep(10)
--- home.java
+++ home.java
@@ -1,13 +1,56 @@
 package git.JavaPyHealthSerch;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 import java.io.*;
-import java.util.*;
+
 
 public class home {
+    public static String result;
     public static void main(String[] args) {
-        Scanner x=new Scanner(System.in);
+        new UserHome();
+    }
+}
+
+class UserHome {
+    public UserHome(){
+        JFrame f=new JFrame("用户界面");
+        f.setBounds(600, 400, 400, 400);
+        JPanel p1=new JPanel(new FlowLayout());
+        
+        JToggleButton xiaoshou=new JToggleButton("消瘦");
+        JToggleButton xiuke=new JToggleButton("休克");
+        
+
+        JButton b1=new JButton("确定");
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                String result="";
+                if (xiaoshou.isSelected()) {
+                    result=result+"1";
+                }
+                if (xiuke.isSelected()) {
+                    result=result+",2";
+                }
+                home.result=result;
+                new InnerPy();
+            }
+        });
+
+
+        p1.add(xiaoshou);
+        p1.add(xiuke);
+        p1.add(b1);
+        f.add(p1);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(3);
+    }
+}
+
+class InnerPy {
+    public InnerPy(){
         try {
             ProcessBuilder pb = new ProcessBuilder("python", "git\\JavaPyHealthSerch\\getdata.py");
             Process process = pb.start();
@@ -17,15 +60,11 @@ public class home {
             OutputStream outputStream = process.getOutputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream));
-
-            System.out.println("请输入序号:");
-            String setin=x.next();
-
-            // 向Python程序发送输入
-            writer.println(setin);
+ 
+            writer.println(home.result);
             writer.flush();
             
-            // 读取输出
+            // 读取python的输出
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
@@ -35,7 +74,6 @@ public class home {
             int exitCode = process.waitFor();
             System.out.println("Python程序执行完成,退出码:" + exitCode);
 
-            x.close();
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
