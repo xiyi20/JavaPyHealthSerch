93cae5c1b093453fe9a94e8db4f4853ab0b9bbe6
dxcxy <wanandsc@gmail.com>
2023-11-30 18:09:44 +0800

    优化结果读取，新增结果查询框

--- MainPro.java
+++ MainPro.java
@@ -8,7 +8,6 @@ import java.io.*;
 
 
 public class MainPro {
-    public static String result;
     public static void main(String[] args) {
         new UserHome();
     }
@@ -16,10 +15,11 @@ public class MainPro {
 
 class UserHome {
     public UserHome(){
+        InnerPy innerpy=new InnerPy();
         JFrame f=new JFrame("用户界面");
-        f.setBounds(600, 400, 400, 400);
+        f.setBounds(600, 200, 600, 400);
         JPanel p1=new JPanel(new FlowLayout());
-        
+        JTextArea tarea=new JTextArea(20, 40);
         JToggleButton xiaoshou=new JToggleButton("消瘦");
         JToggleButton xiuke=new JToggleButton("休克");
         
@@ -27,6 +27,7 @@ class UserHome {
         JButton b1=new JButton("确定");
         b1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
+                tarea.setText("");
                 String result="";
                 if (xiaoshou.isSelected()) {
                     result=result+"1";
@@ -34,8 +35,7 @@ class UserHome {
                 if (xiuke.isSelected()) {
                     result=result+",2";
                 }
-                MainPro.result=result;
-                new InnerPy();
+                tarea.setText(innerpy.getdata(result));
             }
         });
 
@@ -43,6 +43,8 @@ class UserHome {
         p1.add(xiaoshou);
         p1.add(xiuke);
         p1.add(b1);
+        p1.add(tarea);
+
         f.add(p1);
         f.setVisible(true);
         f.setDefaultCloseOperation(3);
@@ -50,32 +52,35 @@ class UserHome {
 }
 
 class InnerPy {
-    public InnerPy(){
+    public InnerPy(){}
+    public String getdata(String sendin){
         try {
-            ProcessBuilder pb = new ProcessBuilder("python", "git\\JavaPyHealthSerch\\getdata.py");
+            ProcessBuilder pb = new ProcessBuilder("python", "git\\JavaPyHealthSerch\\Getdata.py");
             Process process = pb.start();
-            
             // 获取进程的输入流和输出流
             InputStream inputStream = process.getInputStream();
             OutputStream outputStream = process.getOutputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "gbk"));
             PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream));
  
-            writer.println(MainPro.result);
+            writer.println(sendin);
             writer.flush();
             
             // 读取python的输出
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-            
-            // 等待进程执行完成
+            String line;String res="";
             int exitCode = process.waitFor();
-            System.out.println("Python程序执行完成,退出码:" + exitCode);
-
+            int flag=1;
+            while ((line = reader.readLine())!=null) {
+                if(flag%2==0)res+=line+"\n\n";
+                else res+=line+"\n";
+                flag++;
+            }
+            if (exitCode==0) System.out.println("Python程序执行完成");
+            else System.out.println("Python程序执行异常,退出码:"+exitCode);
+            return res;
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+        return "好像有问题";
     }
 }
