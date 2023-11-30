00eaefa4712dfe28eadf6234a5d66ab7d20df207
username <1650504849@qq.com>
2023-11-30 22:10:28 +0800

    mm'

--- MainPro.java
+++ MainPro.java
@@ -4,13 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 import java.io.*;
-//ces
+//eshi
 public class MainPro {
     public static void main(String[] args) {
         new UserHome();
     }
 }
-
 class UserHome {
     public UserHome(){
         InnerPy innerpy=new InnerPy();
