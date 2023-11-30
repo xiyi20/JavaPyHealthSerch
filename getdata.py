3cb18d2f6bef5f49e76ed844b949e8fc40404629
dxcxy <wanandsc@gmail.com>
2023-11-30 18:09:06 +0800

    增加查询结果返回，优化代码

--- getdata.py
+++ getdata.py
@@ -1,14 +1,16 @@
-import time
-from bs4 import BeautifulSoup
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+find_name=re.compile(r'target="_self">(.*?)</a>')
+find_symb=re.compile(r'症状：(.*?)</font>')
 
 option=webdriver.ChromeOptions()
 option.add_argument('--log-level=3')
+option.add_argument("--headless")  # 开启无界面模式
+# option.add_argument("--disable-gpu") 
 
 borwers=webdriver.Chrome(options=option)
-
 borwers.get('https://jibing.bmcx.com/')
 
 buttom=input()
@@ -17,5 +19,11 @@ for i in values:
     if i=='':
         continue
     borwers.find_element(By.XPATH,'//*[@id="x_z_k"]/ul[1]/li['+i+']/label/input').click()
+borwers.find_element(By.ID,'button').click()
 
-time.sleep(10)
+ul=borwers.find_element(By.CLASS_NAME,'list')
+lis=ul.find_elements(By.TAG_NAME,'li')
+for i in lis[:6]:
+    print(i.text)
+
+borwers.quit()
