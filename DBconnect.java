960230db2a05f9fd90dcccd82229fcce7e4fbfa9
dxcxy <wanandsc@gmail.com>
2023-12-01 21:23:40 +0800

    增加登录方法

--- DBconnect.java
+++ DBconnect.java
@@ -1,6 +1,6 @@
 package git.JavaPyHealthSerch;
 import java.sql.*;
-
+ 
 public class DBconnect {
     String jdbc="com.mysql.cj.jdbc.driver";
     String url="jdbc:mysql//localhost:3306/healthserch";
@@ -15,5 +15,22 @@ public class DBconnect {
             e.printStackTrace();
         }
     }
-    
+    public int login(String name,String pw){
+        int exitcode=0;
+        String exec="select password from paintinfo where name='"+name+"'";
+        try {
+            Statement s=c.createStatement();
+            ResultSet r=s.executeQuery(exec);
+            if(r.next()){
+                if (pw.equals(r.getString("password"))==false) {
+                    exitcode=1;//密码错误
+                }
+            }
+            else exitcode=2;//不存在此用户
+        } catch (Exception e) {
+            exitcode=3;//程序错误退出
+            e.printStackTrace();
+        }
+        return exitcode;
+    }
 }
