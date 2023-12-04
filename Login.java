049f843fbefca9421635a373ff8a57fcd8dbb239
dxcxy <wanandsc@gmail.com>
2023-12-04 13:53:46 +0800

    登录页面框架

--- Login.java
+++ Login.java
@@ -1,9 +1,17 @@
-package git.JavaPyHealthSerch;
 
-import java.sql.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login {
-    
+class Login {
+    public Login(){
+        JFrame frame=new JFrame("登录界面");
+
+
+
+        frame.setBounds(650,200,400, 400);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(3);
+    }
 }
