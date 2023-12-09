2625e05a9a56c0b155f6b3c75bb8ce854166e25b
dxcxy <wanandsc@gmail.com>
2023-12-09 19:18:48 +0800

    简单调整

---
+++ ManagerHome.java
@@ -0,0 +1,18 @@
+
+import java.awt.*;
+import javax.swing.*;
+
+class ManagerHome {
+    public ManagerHome(){
+        JFrame frame=new JFrame("后台管理");
+        
+
+
+
+
+
+        frame.setVisible(true);
+        frame.setBounds(300,0,900, 700);
+        frame.setDefaultCloseOperation(3);
+    }
+}
